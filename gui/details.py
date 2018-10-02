b7b93b21a43eeb423ded6be27b65616352a1f97d
alexbagirov <a.d.bagirov@gmail.com>
2018-10-02 23:09:23 +0500

    PEP8

--- gui/details.py
+++ gui/details.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QGroupBox, QGridLayout
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QGroupBox,
+                             QGridLayout)
 from PyQt5.QtGui import QFont
 
 
